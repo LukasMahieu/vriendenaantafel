2a4d11b1a95d23e5664fb8fbbce7372c848fe83d
LukasMahieArinti <Lukas.Mahieu@arinti.ai>
2022-10-15 15:19:21 +0200

    Remove old icon

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import LogoIcon from '../../svg/LogoIcon';
 import Button from '../Button';
+import Icon from "../../../src/assets/logo.svg";
 
 const Header = () => (
   <header className="sticky top-0 bg-yellow-100 shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="w-12 mr-3">
-        <LogoIcon />
+        <Icon />
       </div>
       <div className="flex mt-4 sm:mt-0">
         <AnchorLink className="px-4" href="#home">
--- src/svg/LogoIcon.js
+++
@@ -1,7 +0,0 @@
-import Icon from "../../src/assets/logo.svg";
-
-const LogoIcon = () => (
-  <Icon />
-);
-
-export default LogoIcon;
