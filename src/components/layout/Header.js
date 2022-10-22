fc1133f9e0ac0625d225ec761176aabca08a74f3
LukasMahieArinti <Lukas.Mahieu@arinti.ai>
2022-10-22 15:10:46 +0200

    New component for Hero svg and header text

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,23 +4,23 @@ import Button from '../Button';
 import Icon from "../../../src/assets/logo.svg";
 
 const Header = () => (
-  <header className="border-2 top-0 text-vat-header_text bg-vat-header_background shadow">
-    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
-      <div className="w-12 mr-3">
+  <header className="top-0 mb-5 text-vat-header_text bg-vat-header_background">
+    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-0 px-4">
+      <div className="w-12 mr-3 hidden md:block">
         <Icon />
       </div>
-      <div className="flex mt-4 sm:mt-0">
+      <div className="flex text-l mt-4 sm:mt-0 m:text-2xl ">
         <AnchorLink className="px-4" href="#home">
-          Home
+          HOME
         </AnchorLink>
-        <AnchorLink className="px-4" href="#services">
-          Mijn Keuken
+        <AnchorLink className="px-4" href="#mijnkeuken">
+          MIJN KEUKEN
         </AnchorLink>
         <AnchorLink className="px-4" href="#stats">
-          Formules
+          FORMULES
         </AnchorLink>
         <AnchorLink className="px-4" href="#testimonials">
-          Contact
+          CONTACT
         </AnchorLink>
       </div>
       <a href="https://www.instagram.com/vriendenaantafel/" target="_blank">
