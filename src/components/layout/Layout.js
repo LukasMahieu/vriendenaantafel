4f198dd666d1f27335700a1dcf06dc4f7f73ec84
LukasMahieArinti <Lukas.Mahieu@arinti.ai>
2022-10-23 23:52:12 +0200

    Update font

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header/>
-      <main className="text-vat-smalltext bg-vat-background">{children}</main>
+      <main className="font-vat text-vat-smalltext bg-vat-background">{children}</main>
     </>
   );
 };
