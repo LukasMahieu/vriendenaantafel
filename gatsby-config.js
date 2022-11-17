eb726febb439132a71c1d094ba39fee536b4f908
LukasMahieu <lukas.mahieu@gmail.com>
2022-11-17 12:55:26 +0100

    Add tokens

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,8 @@ module.exports = {
       resolve: `gatsby-source-instagram`,
       options: {
         username: `52415216061`,
+        access_token: 'IGQVJWVHNoV05mdWRLZAUFWSUpweDVBaldJd3pBTmRGLXFEYUkwUFFpM093LXE3WUhTM1Y5M3R0OHVUQTN6VUdDemFlekdPOU5FdTBSbDhyc3dJXzZAzYkhUTXVwcHoydFVwWW16ckxTT1pFWUs2QnkxOAZDZD',
+        instagram_id: '52415216061',
       }
     },
   ]
