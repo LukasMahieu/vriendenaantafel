34940684f64b78c9bc5d23f3451befe9e6a88923
LukasMahieu <lukas.mahieu@gmail.com>
2023-09-30 16:51:36 +0200

    Fix banner query name

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 export default function Banner ({  }) {
   const data = useStaticQuery(graphql`
-  query {
+  query bannerQuery {
     allMarkdownRemark(filter: {frontmatter: {slug: {eq: "banner"}}}) {
       edges {
         node {
@@ -42,31 +42,32 @@ export default function Banner ({  }) {
 
   const image = getImage(data.allMarkdownRemark.edges[0].node.frontmatter.Image01);
   const image_full = getImage(data.allMarkdownRemark.edges[0].node.frontmatter.Image02);
+  console.log(image_full);
   const text = data.allMarkdownRemark.edges[0].node.internal.content;
   const title = data.allMarkdownRemark.edges[0].node.frontmatter.title;
   const subtitle = data.allMarkdownRemark.edges[0].node.frontmatter.subtitle;
 
   return (
     <section id="home">
-      <div class="relative container pb-20 mx-auto flex flex-col-reverse lg:flex-row items-center">
-        <div class="justify-center text-transform: uppercase flex flex-col">
-          <h1 class="text-4xl lg:text-5xl xl:text-6xl font-bold text-vat-bigtext text-center md:text-left">{title}</h1>
-          <h3 class="text-2xl lg:text-2xl xl:text-3xl text-vat-bigtext text-center sm:text-left">{subtitle}</h3>
-          <div class="py-6">
-            <div class="text-center lg:text-left m:text-xl">
+      <div className="relative container pb-20 mx-auto flex flex-col-reverse lg:flex-row items-center">
+        <div className="justify-center text-transform: uppercase flex flex-col">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold text-vat-bigtext text-center md:text-left">{title}</h1>
+          <h3 className="text-2xl lg:text-2xl xl:text-3xl text-vat-bigtext text-center sm:text-left">{subtitle}</h3>
+          <div className="py-6">
+            <div className="text-center lg:text-left m:text-xl">
               <AnchorLink href="#contact">
                 <Button>CONTACT</Button>
               </AnchorLink>
             </div>
-            <div class="pt-0">
+            <div className="pt-0">
             </div>
           </div>
         </div>
-      <div class="w-1/2 py-5 xl:ml-20 lg:py-0 lg:hidden">
-        <GatsbyImage image={image} alt={data.allMarkdownRemark.edges[0].node.frontmatter.image1alt } className="hero-img" style={{ opacity: 1.0 }}/>
+      <div className="w-1/2 py-5 xl:ml-20 lg:py-0 lg:hidden">
+        <GatsbyImage image={image} alt="alt" className="hero-img" style={{ opacity: 1.0 }}/>
       </div>
-      <div class="w-2/3 py-5 px-0 mx-0 overflow-hidden rounded-md lg:py-0 hidden lg:block">
-        <GatsbyImage image={image_full} alt={data.allMarkdownRemark.edges[0].node.frontmatter.image1alt } className="hero-img" style={{ opacity: 1.0 }}/>
+      <div className="w-2/3 py-5 px-0 mx-0 overflow-hidden rounded-md lg:py-0 hidden lg:block">
+        <GatsbyImage image={image_full} alt="alt" className="hero-img" style={{ opacity: 1.0 }}/>
       </div>
       </div>
     </section>
--- src/data/markdown/banner/banner.md
+++ src/data/markdown/banner/banner.md
@@ -2,7 +2,7 @@
 title: 'Vrienden aan tafel'
 subtitle: 'Jij nodigt uit, ik kom koken'
 slug: 'banner'
-Image01: '../../images/banner_round.png'
-Image02: '../../images/banner_full.jpg'
+Image01: ../../images/banner_round.png
+Image02: ../../images/banner_full.jpg
 date: '2022-10-16'
 ---
